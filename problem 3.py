6fa977fb82342a977f55b3f14a993edf9a9663eb
Lokendra Singh Tanwar <46755891+lokendra7512@users.noreply.github.com>
2020-10-01 00:19:16 +0530

    Added Solution for Problem 3

---
+++ problem 3.py
@@ -0,0 +1,13 @@
+def max_factor(num):
+    """Find the maximum prime factor."""
+    factor = 2
+    while factor * factor <= num:
+        while num % factor == 0:
+            num /= factor
+        factor += 1
+    if (num > 1):
+        return num
+    return factor
+    
+
+print(max_factor(600851475143))
